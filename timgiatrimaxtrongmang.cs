17b83c8e372a0d98892ec244aba97c806b70968f
zorozomeo07 <125963795+zorozomeo07@users.noreply.github.com>
2024-03-04 12:04:00 +0800

    phv
    phv

--- timgiatrimaxtrongmang.cs
+++ timgiatrimaxtrongmang.cs
@@ -11,27 +11,21 @@ namespace Wo2
         static void Main(string[] args)
 
         {
-            int size;
+           
             int[] array;
-            do
-            {
-                Console.WriteLine("Enter a size:");
-                size = Int32.Parse(Console.ReadLine());
-                if (size > 5)
-                    Console.WriteLine("Size should not exceed 5");
-            } while (size > 5);
-            array = new int[size];
+            Console.WriteLine("Nhập trí trị cho mảng có 5 phần tử:");
+            array = new int[5];
             int i = 0;
             while (i < array.Length)
             {
-                Console.WriteLine("Enter element" + (i + 1) + " : ");
+                Console.WriteLine("Phần tử " + (i + 1) + " : ");
                 array[i] = Int32.Parse(Console.ReadLine());
                 i++;
             }
-            Console.WriteLine("Property list: ");
+            Console.WriteLine("Mảng giá trị: ");
             for (int j = 0; j < array.Length; j++)
             {
-                Console.WriteLine(array[j] + "\t");
+                Console.Write(array[j] + " ");
             }
             int max = array[0];
             int index = 1;
@@ -43,7 +37,8 @@ namespace Wo2
                     index = j + 1;
                 }
             }
-            Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
+            Console.WriteLine();
+            Console.WriteLine("Giá trị lớn nhất trong mảng trên là :" + max + " , ở vị trí " + index);
             Console.ReadKey();
 
         }
