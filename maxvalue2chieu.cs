514015eef06ab3cab77cbd99719ee46a560bc0ed
zorozomeo07 <125963795+zorozomeo07@users.noreply.github.com>
2024-03-04 13:34:58 +0800

    phv
    phv

---
+++ maxvalue2chieu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            // vd mảng 2 chiều
+            int[,] number = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int max = number[0, 0];
+            int a = 0;int b = 0;
+            for(int i=0; i<number.GetLength(0); i++)
+            {
+                for(int j = 0; j < number.GetLength(1); j++)
+                {
+                    if (number[i, j] > max)
+                    {
+                        max = number[i, j];
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+            Console.WriteLine("Phần tử lớn nhất trong mảng là :" + max+" và ở vị trí dong "+(a+1) +" cột "+(b+1));
+
+
+            Console.ReadKey();
+          
+        }
+    }
+}
